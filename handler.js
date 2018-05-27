2c5cbdb35e1ec936c3de58bf248439e934d22511
sergeome <sergeome@gmail.com>
2018-05-26 21:06:42 -0500

    initial commit

---
+++ handler.js
@@ -0,0 +1,10 @@
+module.exports.endpoint = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Hello World`,
+    }),
+  };
+
+  callback(null, response);
+};
